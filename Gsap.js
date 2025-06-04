9fd3802d10a0cacecd719348cc2596b3e6d3c36a
Esan <ethuntcg@gmail.com>
2025-06-04 23:02:40 +0600

    Updated

--- Gsap.js
+++ Gsap.js
@@ -8,6 +8,108 @@
             init() {
                 this.animateFadeUp();
                 this.animateScale();
+                  this.animateFadeLeft();
+                this.animateFadeRight();
+                   this.animateScale();
+                this.animateRotate();
+                this.animateSlideDown();
+            }
+  // Fade Left Animation
+            animateFadeLeft() {
+                gsap.utils.toArray('.animate-fade-left').forEach(element => {
+                    const delay = element.dataset.delay || 0;
+                    
+                    gsap.to(element, {
+                        opacity: 1,
+                        x: 0,
+                        duration: 1,
+                        delay: delay,
+                        ease: "power2.out",
+                        scrollTrigger: {
+                            trigger: element,
+                            start: "top 80%",
+                            toggleActions: "play none none reverse"
+                        }
+                    });
+                });
+            }
+   // Scale Animation
+            animateScale() {
+                gsap.utils.toArray('.animate-scale').forEach(element => {
+                    const delay = element.dataset.delay || 0;
+                    
+                    gsap.to(element, {
+                        opacity: 1,
+                        scale: 1,
+                        duration: 1,
+                        delay: delay,
+                        ease: "back.out(1.7)",
+                        scrollTrigger: {
+                            trigger: element,
+                            start: "top 80%",
+                            toggleActions: "play none none reverse"
+                        }
+                    });
+                });
+            }
+
+            // Rotate Animation
+            animateRotate() {
+                gsap.utils.toArray('.animate-rotate').forEach(element => {
+                    const delay = element.dataset.delay || 0;
+                    
+                    gsap.to(element, {
+                        opacity: 1,
+                        rotation: 0,
+                        duration: 1,
+                        delay: delay,
+                        ease: "power2.out",
+                        scrollTrigger: {
+                            trigger: element,
+                            start: "top 80%",
+                            toggleActions: "play none none reverse"
+                        }
+                    });
+                });
+            }
+
+            // Slide Down Animation
+            animateSlideDown() {
+                gsap.utils.toArray('.animate-slide-down').forEach(element => {
+                    const delay = element.dataset.delay || 0;
+                    
+                    gsap.to(element, {
+                        opacity: 1,
+                        y: 0,
+                        duration: 1,
+                        delay: delay,
+                        ease: "power2.out",
+                        scrollTrigger: {
+                            trigger: element,
+                            start: "top 80%",
+                            toggleActions: "play none none reverse"
+                        }
+                    });
+                });
+            }
+            // Fade Right Animation
+            animateFadeRight() {
+                gsap.utils.toArray('.animate-fade-right').forEach(element => {
+                    const delay = element.dataset.delay || 0;
+                    
+                    gsap.to(element, {
+                        opacity: 1,
+                        x: 0,
+                        duration: 1,
+                        delay: delay,
+                        ease: "power2.out",
+                        scrollTrigger: {
+                            trigger: element,
+                            start: "top 80%",
+                            toggleActions: "play none none reverse"
+                        }
+                    });
+                });
             }
 
             // Fade Up Animation
